7d2ed958ea4df0edd72f1aecedda42c83f4438b7
Eludini Oladimeji <dimeji.go77@gmail.com>
2024-02-18 10:41:45 +0100

    config

---
+++ nvim/lua/dimeji/plugins/init.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+
+  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+  {"mg979/vim-visual-multi" ,
+  keys = { 
+  { "<C-n>" , "<cmd><C-n>" }
+    },
+}
+}
---
+++ nvim/lua/dimeji/plugins/nvim-treesitter.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+      "HiPhish/nvim-ts-rainbow2",
+    },
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
+
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
+        rainbow = {
+          enable = true,
+          -- list of languages you want to disable the plugin for
+          disable = { 'jsx', 'cpp', 'html' },
+          -- Which query to use for finding delimiters
+          query = 'rainbow-parens',
+          -- Highlight the entire buffer all at once
+          strategy = require('ts-rainbow').strategy.global
+        },
+
+        highlight = {
+          enable = true,
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        autotag = {
+          enable = true,
+        },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "svelte",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+      })
+    end,
+  },
+}
